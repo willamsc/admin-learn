1b530c76a33883274bc68d0c64b49b9e102bb660
willamsc <1196447890@qq.com>
2024-12-12 15:43:11 +0800

    JavaScript中几种常见的输出方式

--- javascript/output.js
+++ javascript/output.js
@@ -1,1 +1,18 @@
-﻿
+﻿//javascript的输出有几种方式
+console.log("hello world");
+//输出变量
+let  a = 1;
+console.log(a);
+//2、输出在HTML中，document.write() 会覆盖页面的内容。
+document.write("hello world");
+
+//3、输出在HTML中，innerHTML 会覆盖页面的内容。
+document.getElementById("demo").innerHTML = "hello world";
+//4、alert 会弹出一个警告框。
+alert("hello world");
+
+//5、confirm 会弹出一个确认框。
+confirm("hello world");
+
+//6、prompt 会弹出一个输入框。获得的数据可以通过变量保存
+let b = prompt("hello world");
