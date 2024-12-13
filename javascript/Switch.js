33024231609684fc190ace243cd95fa139f1d958
willamsc <1196447890@qq.com>
2024-12-13 16:06:30 +0800

    Switch语句的代码

---
+++ javascript/Switch.js
@@ -0,0 +1,26 @@
+﻿let day = 3;
+switch (day) {
+    case 1:
+        console.log("星期一");
+        break;
+    case 2:
+        console.log("星期二");
+        break;
+    case 3:
+        console.log("星期三");
+        break;
+    case 4:
+        console.log("星期四");
+        break;
+    case 5:
+        console.log("星期五");
+        break;
+    case 6:
+        console.log("星期六");
+        break;
+    case 7:
+        console.log("星期天");
+        break;
+    default:
+        console.log("无效的日期");
+}
