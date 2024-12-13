04f5190667febe3e5687e0affb11f2ca3a53f130
willamsc <1196447890@qq.com>
2024-12-13 15:44:11 +0800

    if语句的代码

---
+++ if语句.js
@@ -0,0 +1,30 @@
+﻿let age = 20;
+
+if (age >= 18) {
+    console.log("你是成年人");
+}
+
+
+
+
+if (age >= 18) {
+    console.log("你是成年人");
+} else {
+    console.log("你是未成年人");
+}
+
+
+
+
+
+let score = 85;
+
+if (score >= 90) {
+    console.log("优秀");
+} else if (score >= 70) {
+    console.log("良好");
+} else if (score >= 50) {
+    console.log("及格");
+} else {
+    console.log("不及格");
+}
